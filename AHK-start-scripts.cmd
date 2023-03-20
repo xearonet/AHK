c4dd01365dedf5d40377a7270dacdd80ca6477a9
xearonet <xearonet@o2.pl>
2023-03-20 21:57:15 +0100

    Fix Enabled folder

--- AHK-start-scripts.cmd
+++ AHK-start-scripts.cmd
@@ -6,24 +6,25 @@ REM Version:       2.00
 REM Desc:          Start AutoHotKey scripts
 REM --------------------------------------------------------
 
-cls
+REM cls
 setlocal
 set title=Start AutoHotKey scripts
 title %title% & echo %title%
 echo.
 
 set "ahk_binary=C:\Portable\AutoHotkey_1.1.35.00\AutoHotkeyU64.exe"
-set "logfile=%~dp0\log.txt"
+set "logfile=%~dp0log.txt"
 
 echo %DATE% %TIME% > %logfile%
 echo ------------------------- >> %logfile%
 
 echo Loaded scripts: >> %logfile%
 echo Loaded scripts:
-for %%f in (Enabled\*.ahk) do (
+for %%f in (%~dp0Enabled\*.ahk) do (
     echo     ^-^> %%~nxf >> %logfile%
     echo     ^-^> %%~nxf
     start "" /B %ahk_binary% "%%f"
 )
 echo Done >> %logfile%
 echo Done
+timeout /T 1 /nobreak > NUL
